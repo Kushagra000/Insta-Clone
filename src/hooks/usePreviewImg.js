94aedd19ecde0ad5d45e67388cf661b48dca3e8b
Kushagra Awasthi <Kushagra111awasthi@gmail.com>
2024-01-10 22:06:29 +0530

    first

--- src/hooks/usePreviewImg.js
+++ src/hooks/usePreviewImg.js
@@ -4,6 +4,17 @@ import useShowToast from './useShowToast';
 const usePreviewImg = () => {
   const[selectedFile,setSlectedFile]= useState(null);
   const showToast = useShowToast()
+  const maxFileSizeInBytes= 2 *  1024 * 1024
+
+  const handleImageChange = (e) =>{
+    const file = e.target.files[0]
+    if(file && file.type.startsWith("image/")){
+
+    }else{
+      showToast("Error","Please upload an image file","error")
+      setSlectedFile(null)
+    }
+  }
 }
 
 export default usePreviewImg
